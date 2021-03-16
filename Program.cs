f52cfb2b30af98f7400a99cb6d62390263b07d19
Rodrigo Alves <rodrigorac.2003@gmail.com>
2021-03-16 12:02:43 -0300

    Finalizando conversor Fahrenheit/Celsius

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace FahrenheitCelsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+           string F;
+           double C, C2;
+
+           Console.WriteLine();
+           Console.WriteLine();
+           Console.WriteLine("---------------------------------");
+           Console.WriteLine("Conversor Fahrenheit para Celsius");
+           Console.WriteLine("---------------------------------");
+           Console.WriteLine();
+           Console.WriteLine(" - Digite o valor a ser convertido...");
+           Console.WriteLine();
+
+           F = Console.ReadLine();
+           Console.WriteLine();
+           
+           C = Convert.ToDouble(F);
+           C2 = (C-32)*5/9;
+
+
+           Console.WriteLine("Conversão:");
+           Console.WriteLine($"{C2:N2} °C");
+
+
+
+            
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
